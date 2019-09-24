49b83657c67e4e3cf228bde52264ad305636fbce
nusoff01 <nusoff01@gmail.com>
2019-09-24 09:55:58 -0400

    Updates

---
+++ precache-manifest.1b4a70f39f47c336323f83767efbe321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23162b0db3d3d0c6c33d43631b698e31",
+    "url": "/lightningtalk/index.html"
+  },
+  {
+    "revision": "d8cecdad872b2deb015b",
+    "url": "/lightningtalk/static/css/main.6cdb7aaf.chunk.css"
+  },
+  {
+    "revision": "5c43a1093083762d226a",
+    "url": "/lightningtalk/static/js/2.3fd2e0ce.chunk.js"
+  },
+  {
+    "revision": "d8cecdad872b2deb015b",
+    "url": "/lightningtalk/static/js/main.534180a3.chunk.js"
+  },
+  {
+    "revision": "27294247a29b97d752e0",
+    "url": "/lightningtalk/static/js/runtime~main.643e3a84.js"
+  }
+]);
